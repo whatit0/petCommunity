d4201169a498bd52e0873a94151f571763118a29
Sungho <ddooochii@gmail.com>
2023-12-17 15:44:50 +0900

    Start

---
+++ src/main/java/com/example/petcommunity/PetCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.petcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetCommunityApplication.class, args);
+    }
+
+}
