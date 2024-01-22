4bbd7cb0230996d6f87a9450b3ab3b0c82e4fa9a
Sungho <ddooochii@gmail.com>
2024-01-22 20:15:23 +0900

    feat: 커뮤니티게시판 엔티티

--- src/main/java/com/example/petcommunity/entity/board/DailyBoardEntity.java
+++ src/main/java/com/example/petcommunity/entity/board/DailyBoardEntity.java
@@ -1,12 +1,49 @@
 package com.example.petcommunity.entity.board;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.example.petcommunity.entity.member.MemberEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
-@Table(name = "DailyBoard")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "dailyboard")
 public class DailyBoardEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "daily_no")
+    private int dailyNo;
+
+    @Column(name = "daily_title")
+    private String dailyTitle;
+
+    @Column(name = "daily_content")
+    private String dailyContent;
+
+    @Column(name = "daily_cnt")
+    private int dailyCnt;
+
+    @Column(name = "daily_like")
+    private int dailyLike;
+
+    @CreationTimestamp
+    @Column(name = "daily_date", updatable = false)
+    private LocalDateTime dailyDate;
+
+    @Column(name = "daily_upload")
+    private String dailyUpload;
+
+    @ManyToOne
+    @Column(name = "user_no")
+    private MemberEntity userNo;
 }
--- src/main/java/com/example/petcommunity/entity/board/InfoBoardEntity.java
+++ src/main/java/com/example/petcommunity/entity/board/InfoBoardEntity.java
@@ -1,4 +1,47 @@
 package com.example.petcommunity.entity.board;
 
+import com.example.petcommunity.entity.member.MemberEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "infoboard")
 public class InfoBoardEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "info_no")
+    private int infoNo;
+
+    @Column(name = "info_title")
+    private String infoTitle;
+
+    @Column(name = "info_content")
+    private String infoContent;
+
+    @Column(name = "info_cnt")
+    private int infoCnt;
+
+    @Column(name = "info_like")
+    private int infoLike;
+
+    @CreationTimestamp
+    @Column(name = "info_date", updatable = false)
+    private LocalDateTime infoDate;
+
+    @Column(name = "info_upload")
+    private String infoUpload;
+
+    @ManyToOne
+    @Column(name = "user_no")
+    private MemberEntity userNo;
 }
