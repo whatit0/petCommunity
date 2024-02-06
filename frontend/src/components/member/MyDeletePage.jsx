0f4af9ad689b3412466690919e0bbb979be4723b
Kwangjin <kwangjin5468@naver.com>
2024-02-06 15:08:18 +0900

    Chore : 회원 탈퇴 페이지 수정

--- frontend/src/components/member/MyDeletePage.jsx
+++ frontend/src/components/member/MyDeletePage.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
-import '../../style/MemberPage.css';
+import '../style/MemberPage.css';
 import {useAuth} from '../../AuthContext';
 
 export default function MyUpdatePage() {
