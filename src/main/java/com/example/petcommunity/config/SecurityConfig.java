6d15e8b2d0f620aeb35fee9e2abe59f8558f8775
gukin <wkekrkqk159@naver.com>
2024-02-25 20:12:26 +0900

    chore : health부분 css 수정 및 age 기능 강아지 나이 계산식 수정

--- src/main/java/com/example/petcommunity/config/SecurityConfig.java
+++ src/main/java/com/example/petcommunity/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
                 // 경로별 인가 작업
                 .authorizeHttpRequests(auth -> auth
                         // 모든 권한 허용 (permitAll)
-                        .requestMatchers("/", "/api/register", "/api/login", "/api/check-userId").permitAll()
+                        .requestMatchers("/", "/api/register", "/api/login", "/api/check-userId", "/api/calorie", "/api/age").permitAll()
                         // USER, ADMIN 권한이 있어야 접근 가능
                         .requestMatchers("/api/user/info/{userNo}", "/api/user/update", "/api/user/delete").hasAnyRole("USER", "ADMIN")
                         // ADMIN 권한이 있어야 접근 가능
