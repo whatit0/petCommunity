1a20cd69f8bec8c7a1e3cd4075714330d4df626b
Kwangjin <kwangjin5468@naver.com>
2024-03-06 15:08:42 +0900

    chore : SecurityFilterChain 코드 수정

--- src/main/java/com/example/petcommunity/config/SecurityConfig.java
+++ src/main/java/com/example/petcommunity/config/SecurityConfig.java
@@ -66,7 +66,6 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
         return httpSecurity.build();
     }
 
