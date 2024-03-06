07b063bc5993c23d5b1b03744f5f696da823f7a6
Kwangjin <kwangjin5468@naver.com>
2024-03-06 15:07:29 +0900

    chore : 푸터부분 수정중

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -4,15 +4,6 @@ import '../style/Footer.css';
 function Footer() {
     return (
         <footer className="footer">
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-instagram"></i>
-            </a>
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-twitter"></i>
-            </a>
         </footer>
     );
 }
