acf69c15374c76f5c2bf06e0c39017f62f96aa34
MyCom <ddooochii@gmail.com>
2024-03-11 15:02:26 +0900

    feat: 실종 신고

--- frontend/src/components/missing/PostList.jsx
+++ frontend/src/components/missing/PostList.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./PostList.module.css"
 import MissingShow from "./conten;t/MissingShow";
+import MissingWrite from "./conten;t/MissingWrite";
 
 const PostList = () => {
+
+    const [view, setView] = useState("show");
+
+    const handleButtonClick = () => {
+        setView(view === "show" ? "write" : "show");
+    };
+
     return (
         <div className={styles['container']}>
             <div className={styles['logo']}>
                 실종 신고 센터
             </div>
             <div className={styles['content']}>
-                <MissingShow></MissingShow>
+                {view === "show" ? <MissingShow /> : <MissingWrite />}
             </div>
             <nav className={styles['footer']}>
-                <button> 글 쓰 기 </button>
+                <button onClick={handleButtonClick}> 글 쓰 기 </button>
             </nav>
         </div>
 
--- frontend/src/components/missing/conten;t/MissingWrite.jsx
+++ frontend/src/components/missing/conten;t/MissingWrite.jsx
@@ -4,7 +4,20 @@ import styles from "./MissingWrite.module.css"
 const MissingWrite = () => {
     return (
         <div className={styles['container']}>
+            <div>
+                제목
+            </div>
+            <input/>
 
+            <div>
+                실종 위치
+            </div>
+            <input/>
+
+            <div>
+                실종 설명
+            </div>
+            <input/>
         </div>
     );
 };
