1e9044d1ad0183ecfe18b3fb7667e07cded65e22
MyCom <ddooochii@gmail.com>
2024-03-14 16:41:53 +0900

    feat: MapContainer.jsx

--- frontend/src/components/missing/MapContainer.jsx
+++ frontend/src/components/missing/MapContainer.jsx
@@ -5,43 +5,69 @@ const MapContainer = ({ onLocationSelect }) => {
     const [marker, setMarker] = useState(null);
 
     useEffect(() => {
-        if (!map) { // 지도가 아직 생성되지 않았다면
-            const mapOptions = {
-                center: new window.naver.maps.LatLng(37.5665, 126.9780),
-                zoom: 10,
-            };
-            const newMap = new window.naver.maps.Map('map', mapOptions);
-            setMap(newMap);
-        }
-
-        if (map) { // 지도가 생성된 후에 이벤트 리스너를 추가
-            const clickListener = window.naver.maps.Event.addListener(map, 'click', function (e) {
-                const lat = e.coord.lat();
-                const lng = e.coord.lng();
-
-                if (marker) {
-                    marker.setMap(null);
-                }
-                const markerOption = {
-                    position: new window.naver.maps.LatLng(lat, lng),
-                    map: map,
-                    icon: {
-                        content: '<img src="/dog.svg" alt="마커 아이콘" style="width:30px; height:30px;"/>',
-                        size: new window.naver.maps.Size(33, 33),
-                        origin: new window.naver.maps.Point(0, 0),
-                        anchor: new window.naver.maps.Point(10, 10)
-                    }
+        // 네이버 지도 API 스크립트가 로드되었는지 확인
+        if (window.naver && window.naver.maps) {
+            if (!map) {
+                const mapOptions = {
+                    center: new window.naver.maps.LatLng(37.5665, 126.9780),
+                    zoom: 10,
                 };
-                const newMarker = new window.naver.maps.Marker(markerOption);
-                setMarker(newMarker);
+                const newMap = new window.naver.maps.Map('map', mapOptions);
+                setMap(newMap);
+            }
 
-                onLocationSelect({ lat, lng });
-            });
+            if (map) {
+                const clickListener = window.naver.maps.Event.addListener(map, 'click', function (e) {
+                    const lat = e.coord.lat();
+                    const lng = e.coord.lng();
 
-            // 클린업 함수에서 이벤트 리스너 제거
-            return () => {
-                window.naver.maps.Event.removeListener(clickListener);
-            };
+                    if (marker) {
+                        marker.setMap(null);
+                    }
+                    const markerOption = {
+                        position: new window.naver.maps.LatLng(lat, lng),
+                        map: map,
+                        icon: {
+                            content: '<img src="/dog.svg" alt="마커 아이콘" style="width:30px; height:30px;"/>',
+                            size: new window.naver.maps.Size(33, 33),
+                            origin: new window.naver.maps.Point(0, 0),
+                            anchor: new window.naver.maps.Point(10, 10)
+                        }
+                    };
+                    const newMarker = new window.naver.maps.Marker(markerOption);
+                    setMarker(newMarker);
+
+                    // reverseGeocode 호출 전에 naver.maps.Service 객체가 있는지 확인
+                    if (window.naver.maps.Service) {
+                        window.naver.maps.Service.reverseGeocode({
+                            coords: new window.naver.maps.LatLng(lat, lng),
+                            orders: [
+                                window.naver.maps.Service.OrderType.ADDR,
+                                window.naver.maps.Service.OrderType.ROAD_ADDR
+                            ].join('.')
+                        }, function (status, response) {
+                            if (status !== window.naver.maps.Service.Status.OK) {
+                                return alert('주소를 찾지 못했습니다.')
+                            }
+
+                            var result = response.v2,
+                                item = result.addresses[0];
+
+                            onLocationSelect({ lat, lng, address: item.jibunAddress || item.roadAddress });
+                            return alert('Address ' + item.jibunAddress);
+                        });
+                    } else {
+                        console.error('naver.maps.Service 객체를 찾을 수 없습니다.');
+                    }
+                });
+
+                // 클린업 함수에서 이벤트 리스너 제거
+                return () => {
+                    window.naver.maps.Event.removeListener(clickListener);
+                };
+            }
+        } else {
+            console.error('naver 객체가 정의되지 않았거나 네이버 지도 API가 로드되지 않았습니다.');
         }
     }, [map, onLocationSelect, marker]); // 의존성 배열 수정
 
--- frontend/src/components/missing/content/MissingWrite.jsx
+++ frontend/src/components/missing/content/MissingWrite.jsx
@@ -1,14 +1,20 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import styles from "./MissingWrite.module.css"
 
 const MissingWrite = () => {
     const fileInputRef = useRef();
+    const [selectedValue, setSelectedValue] = useState("");
     const handleButtononClick = () => {
         fileInputRef.current.click();
     }
+    const handleOptionChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
+
     return (
         <form className={styles['form-container']}>
             <div className={styles['container']}>
+
                 <section>
                     <span className={styles['span-title']}> 장소 이름 </span>
                     <div className={styles['input-container']}>
@@ -21,6 +27,18 @@ const MissingWrite = () => {
                 <section>
                     <span className={styles['span-title']}> 실종 위치 </span>
                     <input type="text" className={styles['input-style']} placeholder="장소를 직접 입력하거나 지도에서 클릭하세요."/>
+                </section>
+
+                <section>
+                    <span className={styles['span-title']}> 견종 </span>
+                    <div className={styles['input-container']}>
+                        <select value={selectedValue} onChange={handleOptionChange} className={styles['input-style']}>
+                            <option value="">개</option>
+                            <option value="option1">고양이</option>
+                            <option value="option2">소 동물</option>
+                            <option value="option3">기타</option>
+                        </select>
+                    </div>
                 </section>
 
                 <section>
@@ -42,9 +60,13 @@ const MissingWrite = () => {
                            onChange={(e) => console.log(e.target.files)}
                     />
                     <button onClick={handleButtononClick} type="button" className={styles['file-upload-button']}>
-                        <img src="/picture.svg" alt="Upload" />
+                        <img src="/picture.svg" alt="Upload"/>
                     </button>
                 </section>
+
+                <section>
+                </section>
+
             </div>
         </form>
     );
