92627fbdd9eb29c65438ccdf8a83be5929800d8f
gukin <wkekrkqk159@naver.com>
2024-04-30 17:21:04 +0900

    chore : ec2 배포 단계

--- src/main/java/com/example/petcommunity/config/SecurityConfig.java
+++ src/main/java/com/example/petcommunity/config/SecurityConfig.java
@@ -106,7 +106,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "http://13.211.140.253:3000", "http://13.211.140.253:8080")); // React 앱의 URL
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "http://13.211.140.253:3000", "http://13.211.140.253:8080", "http://pethub2.shop")); // React 앱의 URL
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
--- src/main/java/com/example/petcommunity/controller/member/MemberIdCheckController.java
+++ src/main/java/com/example/petcommunity/controller/member/MemberIdCheckController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.GET, RequestMethod.POST}, allowCredentials = "true")
+@CrossOrigin(origins = "http://pethub2.shop", methods = {RequestMethod.GET, RequestMethod.POST}, allowCredentials = "true")
 public class MemberIdCheckController {
 
     private final MemberIdCheckService memberIdCheckService;
